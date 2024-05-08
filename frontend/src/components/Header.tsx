209752238034bdcbbb052dbd2084b80e9585806f
Denis-Omingo <omingodenis7@gmail.com>
2024-05-08 16:41:45 +0300

    Creating the main nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
 
 export default function Header() {
   return (
@@ -10,6 +11,9 @@ export default function Header() {
         </Link>
         <div className="md:hidden">
             <MobileNav/>
+        </div>
+        <div className="hidden md:block">
+            <MainNav/>
         </div>
       </div>
     </div>
