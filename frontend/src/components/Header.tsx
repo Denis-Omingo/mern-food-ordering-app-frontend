3f738b227a986ef6233f025f05e504bfc3df00ab
Denis-Omingo <omingodenis7@gmail.com>
2024-05-08 16:37:02 +0300

    Creating Header and mobile nav

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
+
+export default function Header() {
+  return (
+    <div className="border-b-2 border-b-orange-500 py-6">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
+            MERN Eats
+        </Link>
+        <div className="md:hidden">
+            <MobileNav/>
+        </div>
+      </div>
+    </div>
+  )
+}
