ee8a4fd0e64b5125d08b0f2da280b0023adc5ff4
Denis-Omingo <omingodenis7@gmail.com>
2024-05-08 15:33:00 +0300

    Connecting to the database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,15 @@
 import express, {Request,Response} from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+
+
+    mongoose.connect(process.env.MONGODB as string)
+    .then(()=>{
+        console.log('Connected to the database')
+    })
+    .catch((error)=>console.log(error))
+
 
 const app=express();
 app.use(express.json());
