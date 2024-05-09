d47006011a9a900b1b24481be71ad6e76c16e7a2
Denis-Omingo <omingodenis7@gmail.com>
2024-05-09 10:14:41 +0300

    adding the landing page

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import Hero from "@/components/Hero";
 import Header from "../components/Header";
 
 type Props={
@@ -7,6 +8,7 @@ const Layout = ({children}:Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header/>
+      <Hero/>
       <div className="container mx-auto flex-1 py-10">
           {children}
       </div>
