1fa744a9a9266389148f33f58a8efe76702181e3
Denis-Omingo <omingodenis7@gmail.com>
2024-05-09 15:16:19 +0300

    Adding Auth0 to the login button

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,9 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react"
 import { Button } from "./ui/button"
 
 
 const MainNav = () => {
+  const {loginWithRedirect}=useAuth0();
   return (
-    <Button variant="ghost" className="font-bold text-[1.2rem] hover:text-orange-500 hover:bg-white">
+    <Button variant="ghost" className="font-bold text-[1.2rem] hover:text-orange-500 hover:bg-white" 
+    onClick={async()=>await loginWithRedirect()}
+    >
         Login
     </Button>
   )
