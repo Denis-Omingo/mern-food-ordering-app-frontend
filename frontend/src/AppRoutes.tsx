f311a2332df7dc4b8fd60dd4888c21b04bb414db
Denis-Omingo <omingodenis7@gmail.com>
2024-05-10 17:50:15 +0300

    Creating an access token with Auth0 for the frontend

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,6 +1,7 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import HomePage from "./pages/HomePage"
+import AuthCallbackPage from "./pages/AuthCallbackPage"
 
 
 const AppRoutes = () => {
@@ -9,6 +10,7 @@ const AppRoutes = () => {
         <Route path='/'element={<Layout>
             <HomePage/>
         </Layout>}/>
+        <Route path='/auth-callback' element={<AuthCallbackPage/>}/>
         <Route path='/user-profile'element={<span>USER PROFILE</span>}/>
         <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
