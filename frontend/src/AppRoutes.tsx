ded02d7da2c1390392c4f0eae26889bb128076e9
Denis-Omingo <omingodenis7@gmail.com>
2024-05-14 12:45:56 +0300

    Creating update user page using Shadcn form hooks

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -2,16 +2,20 @@ import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from "./layouts/Layout"
 import HomePage from "./pages/HomePage"
 import AuthCallbackPage from "./pages/AuthCallbackPage"
+import UserProfilePage from "./pages/UserProfilePage"
 
 
 const AppRoutes = () => {
   return (
       <Routes>
-        <Route path='/'element={<Layout>
+        <Route path='/'element={<Layout showHero >
             <HomePage/>
         </Layout>}/>
         <Route path='/auth-callback' element={<AuthCallbackPage/>}/>
-        <Route path='/user-profile'element={<span>USER PROFILE</span>}/>
+        <Route path='/user-profile'
+        element={<Layout>
+          <UserProfilePage/>
+        </Layout>}/>
         <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
   )
---
+++ frontend/src/pages/UserProfilePage.tsx
@@ -0,0 +1,10 @@
+import UserProfileForm from "@/forms/user-profile-form/UserProfileForm"
+
+
+const UserProfilePage = () => {
+  return (
+   <UserProfileForm/>
+  )
+}
+
+export default UserProfilePage
